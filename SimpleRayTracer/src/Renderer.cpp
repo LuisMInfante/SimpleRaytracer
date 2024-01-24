f837d0fd9916f00ebfcaac035163a2d94e3405c4
Luis Infante <113048160+LuisMInfante@users.noreply.github.com>
2024-01-24 01:47:25 -0500

    Drawing sphere to screen

--- SimpleRayTracer/src/Renderer.cpp
+++ SimpleRayTracer/src/Renderer.cpp
@@ -37,6 +37,7 @@ void Renderer::Render()
 		{ 
 			// Calculate the coordinate of the pixel in the range [0, 1]
 			glm::vec2 coordinate = { (float)x / (float)m_FinalImage->GetWidth() , (float)y / (float)m_FinalImage->GetHeight() };
+			coordinate = coordinate * 2.0f - 1.0f; // Scale to [-1, 1]
 
 			// Update the pixel at the coordinate 
 			m_ImageData[x + y * m_FinalImage->GetWidth()] = UpdatePixel(coordinate);
@@ -49,10 +50,32 @@ void Renderer::Render()
 uint32_t Renderer::UpdatePixel(glm::vec2 coordinate)
 {
 	// Scale coordinates to RBGA values
-	uint8_t r = (uint8_t)(coordinate.x * 255.0f);
-	uint8_t g = (uint8_t)(coordinate.y * 255.0f);
-	uint8_t b = (uint8_t)(0.2f * 255.0f);
+	uint8_t red = (uint8_t)(coordinate.x * 255.0f);
+	uint8_t green = (uint8_t)(coordinate.y * 255.0f);
+	uint8_t blue = (uint8_t)(0.2f * 255.0f);
+
+	float sphereRadius = 0.5f;
+
+	// Set the camera position
+	glm::vec3 CameraPosition(0.0f, 0.0f, 2.0f);
+
+	// Calculate the ray direction from the camera (-1 for forward)
+	glm::vec3 rayDirection(coordinate.x, coordinate.y, -1.0f);
+	rayDirection = glm::normalize(rayDirection);
+
+	// Calculate the ray distance from the camera to the sphere
+	float a = glm::dot(rayDirection, rayDirection);
+	float b = 2.0f * glm::dot(CameraPosition, rayDirection);
+	float c = glm::dot(CameraPosition, CameraPosition) - sphereRadius * sphereRadius;
+
+	float discriminant = b * b - 4.0f * a * c;
+
+	// If the discriminant is negative, the ray does not intersect the sphere
+	if (discriminant < 0.0f)
+	{
+		return 0xff000000;
+	}
 
 	// Return the pixel color value
-	return 0xff000000 | (b << 16) | (g << 8) | r;
+	return 0xff000000 | (blue << 16) | (green << 8) | red;
 }
